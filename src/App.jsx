365dbb05f1f060eb74ac7f8a029e376ba6b0cfb9
anotherinthefire <rongodfreyultra@gmail.com>
2023-06-26 19:48:24 +0800

    xnav-404

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css'
 
-import Navbar from "./components/Navbar"
+import Layout from "./shared/Layout";
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Resume from "./pages/Resume"
 import Portfolio from "./pages/Portfolio"
 import Contact from "./pages/Contact"
-import Project  from "./pages/Project";
+import Project from "./pages/Project";
 import NotFound from "./pages/NotFound";
 import Signin from "./pages/Signin";
 import Account from "./pages/Account";
@@ -20,30 +20,30 @@ function App() {
   return (
     <Router>
       <div className='App'>
-      <AuthContextProvider>
-      <Navbar />
-        <div>
-          <Routes>
-            
-            <Route path="/signin" element={<Signin />}/>
-            <Route path="/account" element={
-              <Protected>
-                <Account />
-              </Protected>
-            }/>
-            
-            <Route path="/" element={<Home />}/>
-            <Route path="/about" element={<About />}/>
-            <Route path="/resume" element={<Resume />}/>
-            <Route path="/project-list" element={<Portfolio />}/>
-            <Route path="/project/:name" element={<Project />}/>
-            <Route path="/contact" element={<Contact />}/>
-            <Route path="*" element={<NotFound />}/>
-          </Routes>
-        </div>
+        <AuthContextProvider>
+
+          <div>
+            <Routes>
+            <Route element={<Layout />}>
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/account" element={
+                <Protected>
+                  <Account />
+                </Protected>
+              } />
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/resume" element={<Resume />} />
+                <Route path="/project-list" element={<Portfolio />} />
+                <Route path="/project/:name" element={<Project />} />
+                <Route path="/contact" element={<Contact />} />
+              </Route>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
         </AuthContextProvider>
-        </div>
-      </Router>
+      </div>
+    </Router>
   )
 }
 
