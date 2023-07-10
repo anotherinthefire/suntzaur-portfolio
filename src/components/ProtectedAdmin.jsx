aa1a6209dc4a2154ead44030dca66f7e1d5659e3
anotherinthefire <rongodfreyultra@gmail.com>
2023-07-10 20:44:29 +0800

    fix-production

---
+++ src/components/ProtectedAdmin.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+import { Navigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+
+const ProtectedAdmin = ({ children }) => {
+  const { user } = UserAuth();
+  const targetUserId = "ENG6gSuRiqZ2sVfMNmKpU9RHREW2";
+
+  if (!user || user.uid !== targetUserId) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+};
+
+export default ProtectedAdmin;
