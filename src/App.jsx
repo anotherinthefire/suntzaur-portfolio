27734aa317d609eeb9002a9caeb6c01d73b5e160
anotherinthefire <rongodfreyultra@gmail.com>
2023-07-10 16:51:01 +0800

    protectedroutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-// import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.css'
+import "./App.css";
 
 import Layout from "./shared/Layout";
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Resume from "./pages/Resume"
-import Portfolio from "./pages/Portfolio"
-import Contact from "./pages/Contact"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 import Project from "./pages/Project";
 import NotFound from "./pages/NotFound";
 import Signin from "./pages/Signin";
@@ -19,46 +18,35 @@ import ManageHome from "./admin/pages/ManageHome";
 import ManageAbout from "./admin/pages/ManageAbout";
 import ManageResume from "./admin/pages/ManageResume";
 import ManageContact from "./admin/pages/ManageContact";
-import ManageProjects from "./admin/pages/ManageProjects"
+import ManageProjects from "./admin/pages/ManageProjects";
+import ProtectedAdmin from "./components/ProtectedAdmin";
 
 function App() {
-
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <AuthContextProvider>
-
-          <div>
-            <Routes>
-              <Route element={<Layout />}>
-                <Route path="/signin" element={<Signin />} />
-                <Route path="/account" element={
-                  <Protected>
-                    <Account />
-                  </Protected>
-                } />
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/resume" element={<Resume />} />
-                <Route path="/project-list" element={<Portfolio />} />
-                <Route path="/project/:name" element={<Project />} />
-                <Route path="/contact" element={<Contact />} />
-
-                <Route path="/manage-home" element={<ManageHome />} />
-                <Route path="/manage-about" element={<ManageAbout />} />
-                <Route path="/manage-resume" element={<ManageResume />} />
-                <Route path="/manage-project-list" element={<ManageProjects />} />
-                <Route path="/manage-contact" element={<ManageContact />} />
-              </Route>
-
-              
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </div>
+          <Layout />
+          <Routes>
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/account" element={<Protected><Account /></Protected>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/project-list" element={<Portfolio />} />
+            <Route path="/project/:name" element={<Project />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/manage-home" element={<ProtectedAdmin><ManageHome /></ProtectedAdmin>} />
+            <Route path="/manage-about" element={<ProtectedAdmin><ManageAbout /></ProtectedAdmin>} />
+            <Route path="/manage-resume" element={<ProtectedAdmin><ManageResume /></ProtectedAdmin>} />
+            <Route path="/manage-project-list" element={<ProtectedAdmin><ManageProjects /></ProtectedAdmin>} />
+            <Route path="/manage-contact" element={<ProtectedAdmin><ManageContact /></ProtectedAdmin>} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </AuthContextProvider>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/admin/pages/ManageContact.jsx
+++ src/admin/pages/ManageContact.jsx
@@ -1,7 +1,11 @@
 const ManageContact = () => {
   return (
     <div className="h-screen">
-        
+      <div className="grid grid-cols-2 grid-rows-2 gap-1">
+        <div >main</div>
+        <div >upload</div>
+        <div className="col-start-2">notes</div>
+      </div>
     </div>
   )
 }
